7f020be139c34278f2939b7b1f9b7b7167f60441
YstaticK <ystatick@users.noreply.github.com>
2025-09-26 23:45:57 +0200

    Fix duplicate imports in MainActivity

--- app/src/main/java/com/example/photoaivideo/MainActivity.kt
+++ app/src/main/java/com/example/photoaivideo/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.photoaivideo
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
