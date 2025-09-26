5d6d6e7466ebc05eedc2d7a9db7228cc75b61510
YstaticK <ystatick@users.noreply.github.com>
2025-09-26 21:27:17 +0200

    Add LibraryActivity with Images and Videos buttons + new Activities

---
+++ app/src/main/java/com/example/photoaivideo/ImagesLibraryActivity.java
@@ -0,0 +1,12 @@
+package com.example.photoaivideo;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImagesLibraryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images_library);
+    }
+}
